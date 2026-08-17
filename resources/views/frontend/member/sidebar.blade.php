6692b88f6182d518756db4616605b2fe0c4ba660
sureshdeveloperwebnox <sureshdeveloperwebnox@gmail.com>
2026-08-17 16:35:34 +0530

    showing Aadhaar verification content without checking the member_verification setting:

--- resources/views/frontend/member/sidebar.blade.php
+++ resources/views/frontend/member/sidebar.blade.php
@@ -176,12 +176,14 @@
                         <span class="aiz-side-nav-text">{{ translate('Change Password') }}</span>
                     </a>
                 </li>
+                @if (get_setting('member_verification') == 1)
                 <li class="aiz-side-nav-item">
                     <a href="{{ route('member.verification') }}" class="aiz-side-nav-link {{ areActiveRoutes(['member.verification']) }}">
                         <i class="las la-file-alt aiz-side-nav-icon"></i>
                         <span class="aiz-side-nav-text">{{ translate('Aadhar Verification') }}</span>
                     </a>
                 </li>
+                @endif
                 <li class="aiz-side-nav-item">
                     <a href="{{ route('profile_settings') }}" class="aiz-side-nav-link">
                         <i class="las la-user aiz-side-nav-icon"></i>
